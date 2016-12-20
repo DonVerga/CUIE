0e5ac5235ee6b659e97f7da9923bf2cb88046dc7
Flowergirl_81 <iris.cipriano@students.fhnw.ch>
2016-12-20 16:31:59 +0100

    Struktur und Umbennenungen

--- src/main/java/ch/fhnw/cuie/HeightControlPane.java
+++ src/main/java/ch/fhnw/cuie/HeightControlPane.java
@@ -1,26 +1,25 @@
 package ch.fhnw.cuie;
 
 import javafx.geometry.*;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.control.SplitPane;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 
 public class HeightControlPane extends Region {
-    private Slider slider;
-    private Label feet , meter;
-    private double test;
-    private double height = 32;
-    private double possitioning = PREFERRED_SIZE - height;
-    private Pane drawingPane,drawingPaneSlider,dummyBuildPain,masterPain;
-    private static final double PREFERRED_SIZE = 300;
-    private static final double MINIMUM_SIZE   = 150;
-    private static final double MAXIMUM_SIZE   = 600;
 
+    // *** Elemente des Custom Control: *** //
+    private Label feet, meter;
+    private Slider slider;
+    private Pane drawingPaneEifeli, drawingPaneSlider, dummyBuildPane, masterPane;
+
+    private double heightRect = 32;
+    private double positioning = PREFERRED_SIZE - heightRect;
+    private static final double PREFERRED_SIZE = 300;
+    private static final double MINIMUM_SIZE = 150;
+    private static final double MAXIMUM_SIZE = 600;
+
+    // Constructor:
     public HeightControlPane() {
         initialSelf();
         initializeControls();
@@ -36,68 +35,69 @@ public class HeightControlPane extends Region {
         String stylesheet = getClass().getResource("style.css").toExternalForm();
         getStylesheets().add(stylesheet);
 
-        Insets padding           = getPadding();
-        double verticalPadding   = padding.getTop() + padding.getBottom();
+        // Resizing für Pane gem. Holz:
+        Insets padding = getPadding();
+        double verticalPadding = padding.getTop() + padding.getBottom();
         double horizontalPadding = padding.getLeft() + padding.getRight();
         setMinSize(MINIMUM_SIZE + horizontalPadding, MINIMUM_SIZE + verticalPadding);
         setPrefSize(PREFERRED_SIZE + horizontalPadding, PREFERRED_SIZE + verticalPadding);
         setMaxSize(MAXIMUM_SIZE + horizontalPadding, MAXIMUM_SIZE + verticalPadding);
     }
+
     @Override
     protected void layoutChildren() {
         super.layoutChildren();
         resize();
     }
+
     private void resize() {
         double width = getWidth() - getInsets().getLeft() - getInsets().getRight();
         double height = getHeight() - getInsets().getTop() - getInsets().getBottom();
         double size = Math.max(Math.min(Math.min(width, height), MAXIMUM_SIZE), MINIMUM_SIZE);
-
         double scalingFactor = size / PREFERRED_SIZE;
 
         if (width > 0 && height > 0) {
-            masterPain.relocate((getWidth() - PREFERRED_SIZE) * 0.5, (getHeight() - PREFERRED_SIZE) * 0.5);
-            masterPain.setScaleX(scalingFactor);
-            masterPain.setScaleY(scalingFactor);
-
+            masterPane.relocate((getWidth() - PREFERRED_SIZE) * 0.5, (getHeight() - PREFERRED_SIZE) * 0.5);
+            masterPane.setScaleX(scalingFactor);
+            masterPane.setScaleY(scalingFactor);
         }
     }
+
     private void initializeControls() {
         slider = new Slider();
         feet = new Label("feet");
         meter = new Label("meter");
-        drawingPane = new Pane();
+        drawingPaneEifeli = new Pane();
         drawingPaneSlider = new Pane();
-        masterPain = new Pane();
-        dummyBuildPain = new Pane();
-
-
-
+        masterPane = new Pane();
+        dummyBuildPane = new Pane();
     }
 
-//test
     private void layoutControls() {
-
-        drawingPane.getStyleClass().addAll("drawingPane");
+        // Verbindung zu style.css
+        drawingPaneEifeli.getStyleClass().addAll("drawingPaneEifeli");
         drawingPaneSlider.getStyleClass().addAll("drawingPaneSlider");
-        dummyBuildPain.getStyleClass().addAll("dummyBuild");
+        dummyBuildPane.getStyleClass().addAll("dummyBuild");
+        dummyBuildPane.getStyleClass().addAll("dummyBuildPane");
+        masterPane.getStyleClass().addAll("master");
+        slider.getStyleClass().addAll("Slider");
+
+        // Slider configurations:
         slider.setOrientation(Orientation.VERTICAL);
         slider.setPrefHeight(PREFERRED_SIZE);
         slider.setMinHeight(PREFERRED_SIZE);
         slider.setMajorTickUnit(200);
         slider.setMax(1000);
         slider.setShowTickLabels(true);
-        slider.getStyleClass().addAll("Slider");
-        drawingPaneSlider.getChildren().addAll(slider);
-        dummyBuildPain.setPrefHeight(height);
-        dummyBuildPain.setLayoutY(possitioning);
-        dummyBuildPain.setLayoutX(150);
-        dummyBuildPain.getStyleClass().addAll("dummyBuildPane");
-        masterPain.getChildren().addAll(drawingPaneSlider,drawingPane,dummyBuildPain);
-        drawingPane.relocate(40,180);
-        masterPain.getStyleClass().addAll("master");
-        getChildren().addAll(masterPain);
 
+        dummyBuildPane.setPrefHeight(heightRect);
+        dummyBuildPane.setLayoutY(positioning); // damit das Dummy-Building nach oben wächst
+        dummyBuildPane.setLayoutX(150);
+        drawingPaneEifeli.relocate(40, 180);
+
+        drawingPaneSlider.getChildren().addAll(slider); // weil der Slider in einer Pane ist!
+        masterPane.getChildren().addAll(drawingPaneSlider, drawingPaneEifeli, dummyBuildPane);
+        getChildren().addAll(masterPane);
     }
 
 
